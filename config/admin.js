c4499e33079044cb976fb3ad514224f42fd4e865
rmkofficial <kmert0@outlook.com>
2024-11-19 22:38:18 +0300

    update admin configuration to use fallback for JWT secret and add users-permissions plugin configuration

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET') || env('ADMIN_AUTH_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
