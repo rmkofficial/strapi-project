4760d5f5fcac5b32e08952d074154d3b6b939739
rmkofficial <kmert0@outlook.com>
2024-11-19 22:58:41 +0300

    add transfer token configuration and set public server URL in admin and server settings

--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Varsayılan host
+  port: env.int('PORT', 1337),  // Varsayılan port
   app: {
-    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']), // Güvenlik anahtarları
   },
+  url: env('SERVER_URL', 'https://strapi-project-xi2f.onrender.com'), // Public URL
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false), // Webhook ilişkileri
   },
 });
