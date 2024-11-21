9f5edbd73a9c753935154b11bf35451b8ea0a760
rmkofficial <kmert0@outlook.com>
2024-11-21 16:17:42 +0300

    remove popular-course content type, controller, router, and service files

--- src/api/popular-course/routes/popular-course.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * popular-course router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::popular-course.popular-course');
