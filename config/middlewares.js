435d293ff6b97bc3f0b82d24b4b97ca65ca7042b
rmkofficial <kmert0@outlook.com>
2024-11-23 11:48:46 +0300

    enhance security and CORS middleware configurations; add content security policy and enable CORS for all origins

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,25 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // Tüm kaynaklara izin ver. Dilerseniz frontend URL'nizi belirtebilirsiniz.
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'], // İzin verilen HTTP metotları
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
