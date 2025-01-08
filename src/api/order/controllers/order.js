0c35864ba6d4077501d6012d0cda08ceb4cec7ff
rmkofficial <kmert0@outlook.com>
2025-01-08 23:24:48 +0300

    feat(order): add userPackage field to order creation and enhance validation

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -8,25 +8,48 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
-    const { userName, userEmail, userPhone, userTC, userBirthDate, course, paymentId } = ctx.request.body.data;
+    const {
+      userName,
+      userEmail,
+      userPhone,
+      userTC,
+      userBirthDate,
+      course,
+      userPackage, // Paket bilgisi
+      paymentId,
+    } = ctx.request.body.data;
 
     // Boş alan kontrolü
-    if (!userName || !userEmail || !userPhone || !userTC || !userBirthDate || !course || !paymentId) {
+    if (
+      !userName ||
+      !userEmail ||
+      !userPhone ||
+      !userTC ||
+      !userBirthDate ||
+      !course ||
+      !userPackage || // Paket kontrolü
+      !paymentId
+    ) {
       return ctx.badRequest("Eksik bilgiler sağlandı.");
     }
 
     try {
+      console.log("Gönderilen veri:", ctx.request.body.data); // Gelen veriyi logla
+
       // Kursu slug üzerinden bul
       const foundCourse = await strapi.entityService.findMany("api::course.course", {
-        filters: { slug: course }, // Burada slug'ı kullanıyoruz
+        filters: { slug: course }, // Slug ile filtre
         limit: 1, // Tek kurs almak için limit
       });
 
       if (!foundCourse || foundCourse.length === 0) {
+        console.log("Slug ile kurs bulunamadı:", course);
         return ctx.notFound("Kurs bulunamadı.");
       }
 
       const courseId = foundCourse[0].id;
+
+      console.log("Bulunan kurs ID'si:", courseId); // Kurs ID'sini logla
 
       // Siparişi oluştur
       const order = await strapi.entityService.create("api::order.order", {
@@ -37,9 +60,12 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
           userTC,
           userBirthDate,
           course: courseId,
+          userPackage, // Paket bilgisi kaydediliyor
           paymentId,
         },
       });
+
+      console.log("Sipariş başarıyla oluşturuldu:", order); // Sipariş detaylarını logla
 
       return ctx.created(order);
     } catch (error) {
